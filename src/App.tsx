821df3cf552162a6140d3ec55d48cb270f8114e1
HSCHELO <knr0013@gmail.com>
2024-06-02 01:54:42 +0900

    feat:메인페이지 레이아웃 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import SeudeumePage from "./pages/SeudeumePage";
+import Mainpage from "./pages/Mainpage";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Header />
         <Routes>
           {/* 나중에 여기에 메인페이지 넣읍시다 */}
-          <Route path="/" element={<SeudeumePage />} />
+          <Route path="/" element={<Mainpage />} />
           <Route path="/Seudeume" element={<SeudeumePage />} />
         </Routes>
       </div>
