c9ab322db7b35f706134173776608ec828f7a608
HSCHELO <knr0013@gmail.com>
2024-06-16 02:56:32 +0900

    feat:달력 구현

--- src/pages/Planner_modal.tsx
+++ src/pages/Planner_modal.tsx
@@ -1,4 +1,15 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FaCalendarAlt } from "react-icons/fa";
+import ko from "date-fns/locale/ko";
+import { registerLocale } from "react-datepicker";
+
+registerLocale("ko", ko);
+
 const Planner_modal = ({ IsModal }) => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
   if (IsModal == true) {
     return (
       <div className="fixed top-0 h-full w-140 bg-gray-100 bg-opacity-80 flex items-center justify-center">
@@ -8,6 +19,16 @@ const Planner_modal = ({ IsModal }) => {
           <p className="text-sm p-4 text-gray-500">
             방문을 희망하시는 날짜를 선택하시면 예약 가능시간이 표시됩니다.
           </p>
+          <div className="flex items-center mt-4">
+            <DatePicker
+              selected={selectedDate}
+              locale="ko"
+              className="p-2 border border-gray-300 rounded-md"
+              onChange={(date) => setSelectedDate(date)}
+              placeholderText="연도,월,일"
+              minDate={new Date()} // 오늘 이전의 날짜는 선택할 수 없도록 설정
+            />
+          </div>
         </div>
       </div>
     );
